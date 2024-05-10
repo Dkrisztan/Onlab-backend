ff965c04fdea5bc46519f5c376bd8730f3c6feb2
Dkrisztan <krisztidancs@gmail.com>
2024-05-10 09:21:56 +0200

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://onlab-frontend-ten.vercel.app/'],
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-    credentials: true,
-  });
+  app.enableCors();
 
   // used for DTO validation
   app.useGlobalPipes(
